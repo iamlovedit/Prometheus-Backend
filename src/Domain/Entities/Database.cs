b394c8e820aab512e9ab3323b856f3db5369fbe7
Young <iamlovedit@163.com>
2024-04-22 20:05:08 +0800

    added new entities

---
+++ src/Domain/Entities/Database.cs
@@ -0,0 +1,22 @@
+﻿using SqlSugar;
+
+namespace Prometheus.Backend.Domain.Entities;
+
+[SugarTable("tenant_database")]
+public class Database : IDeletable
+{
+    [SugarColumn(IsPrimaryKey = true, ColumnName = "database_id")]
+    public long Id { get; set; }
+
+    [SugarColumn(ColumnName = "database_connectionString", IsNullable = false)]
+    public string ConnectionString { get; set; }
+
+    [SugarColumn(ColumnName = "database_productionId")]
+    public long ProductionId { get; set; }
+
+    [Navigate(NavigateType.OneToOne, nameof(ProductionId))]
+    public Production Production { get; set; }
+
+    [SugarColumn(ColumnName = "database_isDeleted")]
+    public bool IsDeleted { get; set; }
+}
---
+++ src/Domain/Entities/Production.cs
@@ -0,0 +1,16 @@
+﻿using SqlSugar;
+
+namespace Prometheus.Backend.Domain.Entities;
+
+[SugarTable("tenant_production")]
+public class Production : IDeletable
+{
+    [SugarColumn(IsPrimaryKey = true,ColumnName = "production_id")]
+    public long Id { get; set; }
+    
+    [SugarColumn(ColumnName = "production_name")]
+    public string Name { get; set; }
+
+    [SugarColumn(ColumnName = "production_isDeleted")]
+    public bool IsDeleted { get; set; }
+}
